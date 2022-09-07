d58f7ba05cd011b17d4a1b3aa1f21870498a715c
Olga <politykoolga@outlook.com>
2022-09-07 10:40:42 +0300

    Three commit

---
+++ Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int []array = new int [N];
+
+int index = 0;
+int num = 0;
+
+while (index < N)
+{
+    array[index] = num * num * num;
+    Console.Write(array[index] + " ");
+    num ++;
+    index ++;
+}
